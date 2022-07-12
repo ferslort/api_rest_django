30e4b4af564ee34bf137f59ea6ef85edc73bdcee
Fernando Padilla <fernandoslort@gmail.com>
2022-07-12 15:26:53 -0400

    update:  projects/models.py

--- projects/models.py
+++ projects/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from user.models import User
+from django.db.models import SET_NULL
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    slug = models.SlugField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    dateDelivery = models.DateField(null=True)
+    description = models.TextField(blank=True)
+    is_disabled = models.BooleanField(default=False)
+    isCompleted = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=SET_NULL, null=True)
 
     def __str__(self):
         return self.name
