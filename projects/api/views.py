88084ce86d3a4d8975252e9f55dda07a24d7f9d2
Fernando Padilla <fernandoslort@gmail.com>
2022-07-12 15:27:35 -0400

    update: projects/api/views.py

--- projects/api/views.py
+++ projects/api/views.py
@@ -1,4 +1,4 @@
-from projects.api.serializer import ProjectsSerializer
+from projects.api.serializer import ProjectSerializer
 from projects.models import Project
 from rest_framework.viewsets import ModelViewSet
 from projects.api.permissions import IsAdminOrReadOnly
@@ -6,5 +6,5 @@ from projects.api.permissions import IsAdminOrReadOnly
 
 class ProjectsView(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
-    serializer_class = ProjectsSerializer
+    serializer_class = ProjectSerializer
     queryset = Project.objects.all()
