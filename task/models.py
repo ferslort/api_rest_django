42ea0d76f83a87bc8660542c4170637fbf5929c5
Fernando Padilla <fernandoslort@gmail.com>
2022-07-12 15:19:16 -0400

    update:task/models.py

--- task/models.py
+++ task/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-
-# Create your models here.
+from user.models import User
+from django.db.models import SET_NULL, CASCADE
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(blank=True, null=False)
     slug = models.SlugField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    project_id = models.CharField(max_length=255)
+    isCompleted = models.BooleanField(default=False)
+    priority = models.IntegerField(default=0, null=False)
+    dateDelivery = models.DateField(null=True)
+    project = models.ForeignKey(
+        to='projects.Project', on_delete=CASCADE, null=False, related_name='tasks')
 
     def __str__(self):
         return self.name
